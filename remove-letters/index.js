d768acc654cf6fbd1653eb6ef494639d58c91f21
skosana721 <wonganiskosana@gmail.com>
2021-04-16 11:56:04 +0200

    created a function that removes letters in an array that has numbers and string in it

---
+++ remove-letters/index.js
@@ -0,0 +1,12 @@
+let array = ["b ", 3, 98, "c", 2, 5, "eat", 11, 7, "car", "string"];
+
+const removeLetters = (arr) => {
+  let numberArray = [];
+  for (let i of arr) {
+    if (typeof i === "number") {
+      numberArray.push(i);
+    }
+  }
+  return numberArray;
+};
+console.log(removeLetters(array));
