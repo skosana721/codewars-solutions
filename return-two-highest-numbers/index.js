ad7a3d582c19ed31f13d4e5e347bc7f93eb11fb8
skosana721 <wonganiskosana@gmail.com>
2021-04-16 14:56:35 +0200

    create a function that find the two highest number in the array

--- return-two-highest-numbers/index.js
+++ return-two-highest-numbers/index.js
@@ -1,29 +1,9 @@
 let array = [2, 1, 6, 4, 40, 1, -3];
-// solution one
 
-// const findHighestNumbers = (arr) => {
-//   let twoHighest = [];
-//   twoHighest.push(arr.sort((a, b) => b - a)[0]);
-//   twoHighest.push(arr.sort((a, b) => b - a)[1]);
-//   return twoHighest;
-// };
-// findHighestNumbers(array);
-
-// solution two
-
-const returnTwoHighest = (num) => {
-  let highest = 0;
-  for (let i in num) {
-    for (let j = 0; j < i; j++) {
-      console.log("passed");
-    }
-    if (highest < num[i]) {
-      highest = num[i];
-      if (highest < num[i]) {
-        highest = num[i];
-      }
-    }
-    return highest;
-  }
+const findHighestNumbers = (arr) => {
+  let twoHighest = [];
+  twoHighest.push(arr.sort((a, b) => b - a)[0]);
+  twoHighest.push(arr.sort((a, b) => b - a)[1]);
+  return twoHighest;
 };
-console.log(returnTwoHighest(array));
+findHighestNumbers(array);
